8f9d9da5795166438ea91391b0b8349ceae2221e
RituAshok <64728891+RituAshok@users.noreply.github.com>
2023-09-13 16:15:53 +0000

    conversion of DT

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,71 @@
-let age = 18
+// let age = 18
+// console.log(typeof age)
+// console.log(typeof(age))
 
-console.log(typeof age)
-console.log(typeof(age))
+// let score = "25"
+// console.log(typeof(score))
+// console.log(typeof score)
 
-let score = "25"
-console.log(typeof(score))
-console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+// let score1 = "123aa"
+// let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1)
+// console.log( valueInNumber1)    //NaN
+
+// let score2=null
+// console.log(typeof score2)
+// let valueInNumber3=Number(score2)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)    //0
+
+// let score3=undefined
+// console.log(typeof score3)
+// let valueInNumber4=Number(score3)
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4)    //NaN
+
+// let score4=true
+// console.log(typeof score4)
+// let valueInNumber5=Number(score4)
+// console.log(typeof valueInNumber5)
+// console.log(valueInNumber5)    //1
+
+// let score5="Ritu"
+// console.log(typeof score5)
+// let valueInNumber6=Number(score5)
+// console.log(typeof valueInNumber6)
+// console.log(valueInNumber6)    //NaN
+
+//"25" => 25
+//"123aa" => NaN   bt typeof NaN is number
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)       //true
+
+// let isLoggedIn1 = ""
+
+// let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+// console.log(typeof booleanIsLoggedIn1)
+// console.log(booleanIsLoggedIn1)    //false
+
+// let isLoggedIn2 = "Ritu"
+
+// let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+// console.log(typeof booleanIsLoggedIn2)
+// console.log(booleanIsLoggedIn2)   //true
+
+//1 => true; 0 => false
+// "" => false
+//"Ritu" => true
+
+// let someNumber = 25
+
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)   //string
+// console.log(stringNumber)    //25
