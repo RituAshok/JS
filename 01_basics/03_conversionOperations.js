e197bff8752d593249a7c4175e167b37568466c0
RituAshok <64728891+RituAshok@users.noreply.github.com>
2023-09-13 12:35:52 +0000

    chnges

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let age = 18
+
+console.log(typeof age)
+console.log(typeof(age))
+
+let score = "25"
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
