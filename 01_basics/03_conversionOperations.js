d46536c4a4bf7ce8aed5566dc61ec18bea30f287
RituAshok <64728891+RituAshok@users.noreply.github.com>
2023-09-13 16:32:14 +0000

    d

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -69,3 +69,32 @@
 // let stringNumber = String(someNumber)
 // console.log(typeof stringNumber)   //string
 // console.log(stringNumber)    //25
+
+
+// ******************** Operations ********************
+
+let value = 3
+let negativeValue = -value
+// console.log (negativeValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Ritu"
+
+let str3 = str1+ str2
+// console.log(str3)   //Hello Ritu
+
+// console.log("1" + 2)   //12
+// console.log(1 + "2")   //12
+// console.log("1" + 2 + 2)   //122
+// console.log(1 + 2 + "2")   //32
+
+// console.log((3 + 4) * 5 % 3)  //2; always use paranthesis for easy use
+
+console.log(true)
