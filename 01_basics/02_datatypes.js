71be9c032a9c91797535767409fd5102c819b6e2
RituAshok <64728891+RituAshok@users.noreply.github.com>
2023-09-13 12:13:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer js version; its not necessary to write this; it automatically works in strict mode
+
+//we are using nodejs, not browser
+// alert("hello")
+// alert (3+3)
+
+console.log(3
+    +
+    3)  //code readablity should be high
+console.log("Ritu")
+
+let name = "Ritu"
+let age= 18
+let isLoggedIn = true
+
+//PRIMITIVE DATA TYPES
+//number => 2 to power 53
+//bigint ; mostly used in trading, stock market, reddit, fb
+//string => ""
+//boolean => true / false
+//null => standalone value; representation of empty value; let state=null
+//undefined => var declared bt value not defined
+//symbol ; for uniqueness; used in components in react 
+
+//object
+
+console.log(typeof "Ritu")
+console.log(typeof age)
+console.log(typeof null)   //object 
+console.log(typeof undefined)   //undefined becoz it is a type
